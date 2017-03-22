f7ea37acfb48d0ea1eeab3023cac651265bca566
mrmiguez <r.m.miguez@gmail.com>
2017-03-22 13:49:26 -0400

    testing

--- runFlaLD.py
+++ runFlaLD.py
@@ -7,15 +7,19 @@ from os.path import abspath, dirname, join
 from FlaLD import FlaLD_DC, FlaLD_MODS, FlaLD_QDC
 
 PATH = join(abspath(dirname(__file__)))
+print(PATH)
 
 def write_json_ld(docs):
     with open(PATH + 'all-sample2.json', 'a') as jsonOutput:
         json.dump(docs, jsonOutput, indent=2)        
 
-for file in glob.glob(PATH + 'debug/test_data/*Small.xml'):
-    if 'fiu' in file:
-        write_json_ld(FlaLD_DC(file))
-    elif 'fsu' in file:
-        write_json_ld(FlaLD_MODS(file))
-    elif 'umiami' in file:
-        write_json_ld(FlaLD_QDC(file))
+for file in glob.glob('./debug/test_data/*Small.xml'):
+    if 'DCdebug' in file:
+        #print(abspath(file)) #test
+        write_json_ld(FlaLD_DC(abspath(file)))
+    elif 'MODSdebug' in file:
+        #print(abspath(file)) #test
+        write_json_ld(FlaLD_MODS(abspath(file)))
+    elif 'QDCdebug' in file:
+        #print(abspath(file)) #test
+        write_json_ld(FlaLD_QDC(abspath(file)))
